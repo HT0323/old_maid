741952681ac05b79cce63ca5b934e636781fae74
HaraT0323 <alra9si916323@gmail.com>
2022-08-14 17:06:23 +0900

    次にカードを引くプレイヤー、引かれるプレイヤーの判定機能の追加

--- main.py
+++ main.py
@@ -65,19 +65,6 @@ def initial_deal(initial_deck: list, *args: tuple()) -> list:
     return players
 
 
-initial_deck = create_initial_deck()
-
-player1 = create_player("test", is_auto=False)
-player2 = create_player("test2")
-player3 = create_player("test3")
-
-
-players = initial_deal(initial_deck, player1, player2, player3)
-deck1 = players[0]["deck"]
-deck2 = players[1]["deck"]
-deck3 = players[2]["deck"]
-
-
 def initial_putdown(deck: list) -> list:
     """初期手札を重複しているカードを捨てる
 
@@ -98,8 +85,48 @@ def initial_putdown(deck: list) -> list:
     return deck
 
 
-initial_putdown(deck1)
-initial_putdown(deck2)
-initial_putdown(deck3)
+def create_turn_index(passer_i: int, taker_i: int, players: list) -> tuple():
+    """次にカードを引くプレイヤー、引かれるプレイヤーのindex情報を作成
 
-print(deck1, deck2, deck3)
+    Args:
+        passer_i (int): 現在のターンのカードを引いたプレイヤーのindex情報
+        taker_i (int): 現在のターンのカードを引かれたプレイヤーのindex情報
+        passer_i (int): プレイヤーのの一覧情報
+
+    Returns:
+        tuple: 次にカードを引くプレイヤー、引かれるプレイヤーのindex情報
+    """
+    passer_i = taker_i
+    taker_i = taker_i + 1
+
+    if passer_i >= len(players):
+        passer_i = 0
+        taker_i = 1
+    elif taker_i >= len(players):
+        taker_i = 0
+
+    return passer_i, taker_i
+
+
+initial_deck = create_initial_deck()
+player1 = create_player("test", is_auto=False)
+player2 = create_player("test2")
+player3 = create_player("test3")
+players = initial_deal(initial_deck, player1, player2, player3)
+for i in range(len(players)):
+    players[i]["deck"] = initial_putdown(players[i]["deck"])
+print(player1["deck"])
+print(player2["deck"])
+print(player3["deck"])
+
+
+players = ["Green", "Yellow", "Red"]
+
+passer_i = 0
+taker_i = 1
+
+passer_i, taker_i = create_turn_index(passer_i, taker_i, players)
+print(f"{players[passer_i]} ==> {players[taker_i]}")
+
+passer_i, taker_i = create_turn_index(passer_i, taker_i, players)
+print(f"{players[passer_i]} ==> {players[taker_i]}")
